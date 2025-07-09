d7d3d555691b229d17a1687ee28acb276c2e83cf
LADS Translation Project <ladstranslation@gmail.com>
2025-07-09 17:43:04 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,117 +1,116 @@
-var querystring = require('querystring');
+// api/index.js
 
-var got = require('got');
-var safeEval = require('safe-eval');
-var token = require('google-translate-token');
+const querystring = require('querystring');
+const got = require('got');
+const safeEval = require('safe-eval');
+const token = require('google-translate-token');
 
-var languages = require('./languages');
+const languages = require('./languages');
 
 function translate(text, opts) {
-    opts = opts || {};
+  opts = opts || {};
 
-    var e;
-    [opts.from, opts.to].forEach(function (lang) {
-        if (lang && !languages.isSupported(lang)) {
-            e = new Error();
-            e.code = 400;
-            e.message = 'The language \'' + lang + '\' is not supported';
-        }
-    });
-    if (e) {
-        return new Promise(function (resolve, reject) {
-            reject(e);
-        });
+  let e;
+  [opts.from, opts.to].forEach((lang) => {
+    if (lang && !languages.isSupported(lang)) {
+      e = new Error();
+      e.code = 400;
+      e.message = `The language '${lang}' is not supported`;
     }
+  });
+  if (e) {
+    return Promise.reject(e);
+  }
 
-    opts.from = opts.from || 'auto';
-    opts.to = opts.to || 'en';
+  opts.from = opts.from || 'auto';
+  opts.to = opts.to || 'en';
 
-    opts.from = languages.getCode(opts.from);
-    opts.to = languages.getCode(opts.to);
+  opts.from = languages.getCode(opts.from);
+  opts.to = languages.getCode(opts.to);
 
-    return token.get(text).then(function (token) {
-        var url = 'https://translate.google.com/translate_a/single';
-        var data = {
-            client: 't',
-            sl: opts.from,
-            tl: opts.to,
-            hl: opts.to,
-            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
-            ie: 'UTF-8',
-            oe: 'UTF-8',
-            otf: 1,
-            ssel: 0,
-            tsel: 0,
-            kc: 7,
-            q: text
+  return token.get(text)
+    .then((token) => {
+      const url = 'https://translate.google.com/translate_a/single';
+      const data = {
+        client: 't',
+        sl: opts.from,
+        tl: opts.to,
+        hl: opts.to,
+        dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+        ie: 'UTF-8',
+        oe: 'UTF-8',
+        otf: 1,
+        ssel: 0,
+        tsel: 0,
+        kc: 7,
+        q: text
+      };
+      data[token.name] = token.value;
+
+      return `${url}?${querystring.stringify(data)}`;
+    })
+    .then((url) => {
+      return got(url).then((res) => {
+        const result = {
+          text: '',
+          from: {
+            language: {
+              didYouMean: false,
+              iso: ''
+            },
+            text: {
+              autoCorrected: false,
+              value: '',
+              didYouMean: false
+            }
+          },
+          raw: ''
         };
-        data[token.name] = token.value;
 
-        return url + '?' + querystring.stringify(data);
-    }).then(function (url) {
-        return got(url).then(function (res) {
-            var result = {
-                text: '',
-                from: {
-                    language: {
-                        didYouMean: false,
-                        iso: ''
-                    },
-                    text: {
-                        autoCorrected: false,
-                        value: '',
-                        didYouMean: false
-                    }
-                },
-                raw: ''
-            };
+        var body = safeEval(res.body);
 
-            if (opts.raw) {
-                result.raw = res.body;
-            }
-
-            var body = safeEval(res.body);
-            body[0].forEach(function (obj) {
-                if (obj[0]) {
-                    result.text += obj[0];
-                }
-            });
-
-            if (body[2] === body[8][0][0]) {
-                result.from.language.iso = body[2];
-            } else {
-                result.from.language.didYouMean = true;
-                result.from.language.iso = body[8][0][0];
-            }
-
-            if (body[7] && body[7][0]) {
-                var str = body[7][0];
-
-                str = str.replace(/<b><i>/g, '[');
-                str = str.replace(/<\/i><\/b>/g, ']');
-
-                result.from.text.value = str;
-
-                if (body[7][5] === true) {
-                    result.from.text.autoCorrected = true;
-                } else {
-                    result.from.text.didYouMean = true;
-                }
-            }
-
-            return result;
-        }).catch(function (err) {
-            var e;
-            e = new Error();
-            if (err.statusCode !== undefined && err.statusCode !== 200) {
-                e.code = 'BAD_REQUEST';
-            } else {
-                e.code = 'BAD_NETWORK';
-            }
-            throw e;
+        body[0].forEach((obj) => {
+          if (obj[0]) {
+            result.text += obj[0];
+          }
         });
+
+        if (body[2] === body[8][0][0]) {
+          result.from.language.iso = body[2];
+        } else {
+          result.from.language.didYouMean = true;
+          result.from.language.iso = body[8][0][0];
+        }
+
+        if (body[7] && body[7][0]) {
+          let str = body[7][0];
+          str = str.replace(/<b><i>/g, '[').replace(/<\/i><\/b>/g, ']');
+
+          result.from.text.value = str;
+          if (body[7][5] === true) {
+            result.from.text.autoCorrected = true;
+          } else {
+            result.from.text.didYouMean = true;
+          }
+        }
+
+        return result;
+      });
     });
 }
 
-module.exports = translate;
-module.exports.languages = languages;
+export default async function handler(req, res) {
+  const { text, from, to } = req.query;
+
+  if (!text) {
+    return res.status(400).json({ error: 'Missing `text` parameter' });
+  }
+
+  try {
+    const result = await translate(text, { from, to });
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message || 'Internal error' });
+  }
+}
