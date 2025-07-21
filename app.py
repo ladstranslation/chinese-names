03de6f795bb330b58f6a7d63665b01a023b15e75
LADS Translation Project <ladstranslation@gmail.com>
2025-07-21 19:47:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4610,4 +4610,6 @@ def api_stats():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
